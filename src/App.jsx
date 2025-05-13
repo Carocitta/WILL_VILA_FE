1e7b378d1c5ced065493f2a0362e6f6998d699ab
carocitta <carocitta@gmail.com>
2025-05-13 19:17:55 +0200

    home page fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage/Home";
-
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div>
+    <div className="container mt-4">
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-
       </Routes>
     </div>
   );
--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,14 +1,19 @@
-import Header from "../../components/Header/Header";
 import Button from "../../components/Button/Button";
 import "./Home.css";
 
 function Home() {
   return (
     <>
-      <Header />
-
       <div className="home-background">
-        <div className="home-content">
+        <div className="home-center">
+          <div className="home-image-container">
+            <h1 className="home-title">WILL VILA</h1>
+            <img
+              src="/img/enfoque_grey.png"
+              alt="Enfoque"
+              className="home-image"
+            />
+          </div>
           <Button
             type="button"
             className="custom-button btn-lg"
@@ -17,7 +22,6 @@ function Home() {
           />
         </div>
       </div>
-    
     </>
   );
 }
